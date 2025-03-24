227a167a15578982ac8454e9d927e4fa2de45dbc
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2025-03-24 15:26:16 -0300

    feature: url dinamica

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,7 +5,7 @@ from recipes import views
 app_name ='recipes'
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>', views.recipe),
+    path('', views.home, name="home"),
+    path('recipes/<int:id>', views.recipe, name="recipe"),
 
 ]
