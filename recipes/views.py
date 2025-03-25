fa442c1d8cf5eddc1b0fedc5636d87835dc589f7
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2025-03-25 15:53:45 -0300

    feature: template de category

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 from .models import Recipe
@@ -7,8 +8,10 @@ from .models import Recipe
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
-        )
-    return render(request, "recipes/pages/home.html", context={'recipes':recipes})
+        ).order_by('-id')
+    return render(request, "recipes/pages/home.html", context={
+        'recipes':recipes,
+        })
 
 
 def recipe(request, id):
@@ -21,6 +24,12 @@ def recipe(request, id):
 def category(request, category_id):
     recipes = Recipe.objects.filter(
         category__id=category_id,
-        is_published=True
-        )
-    return render(request, "recipes/pages/home.html", context={'recipes':recipes})
+        ).order_by('-id')
+
+    if not recipes:
+        raise Http404('Not Found.')
+    
+    return render(request, "recipes/pages/category.html", context={
+        'recipes':recipes,
+        'title': f'{recipes.first().category.name} - Category'
+        })
