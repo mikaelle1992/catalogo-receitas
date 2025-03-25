2a4bf48a6b3fef63b895197246c931fee2dec184
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2025-03-25 18:07:59 -0300

    feature: get_object_or_404

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import get_list_or_404, render, get_object_or_404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
 
@@ -15,26 +15,18 @@ def home(request):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(id=id).first()
+    recipe = get_object_or_404(Recipe.objects.filter(id=id).first())
     return render(request, "recipes/pages/recipe-view.html", context={
         'recipe':recipe,
         'is_datail_page':True
         })
 
 def category(request, category_id):
-    # recipes = Recipe.objects.filter(
-    #     category__id=category_id,
-    #     ).order_by('-id')
-
-    # if not recipes:
-    #     raise Http404('Not Found.')
-    
     recipes = get_list_or_404(
             Recipe.objects.filter(
             category__id=category_id,
             ).order_by('-id')
         )
-
     return render(request, "recipes/pages/category.html", context={
         'recipes':recipes,
         'title': f'{recipes[0].category.name} - Category'
