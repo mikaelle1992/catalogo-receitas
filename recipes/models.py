0af696a8195181ca4b99965db118ea02feac4285
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2025-03-25 15:53:02 -0300

    fix: author

--- recipes/models.py
+++ recipes/models.py
@@ -24,7 +24,8 @@ class Recipe(models.Model):
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null= True)
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null= True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null= True, blank=True,
+        default=None)
 
     def __str__(self):
         return self.title
