3b807376600f2ad0aef357ac9cc177fe0b2ee746
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2025-03-25 14:00:57 -0300

    fix: url dinamica e config da pasta media

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from .models import Recipe
 
 # Create your views here.
 
 def home(request):
-    return render(request, "recipes/pages/home.html", context={'recipes':[make_recipe() for _ in range(10)]})
+    recipes = Recipe.objects.filter(
+        is_published=True
+        )
+    return render(request, "recipes/pages/home.html", context={'recipes':recipes})
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(id=id).first()
     return render(request, "recipes/pages/recipe-view.html", context={
-        'recipe':make_recipe(),
+        'recipe':recipe,
         'is_datail_page':True
         })
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True
+        )
+    return render(request, "recipes/pages/home.html", context={'recipes':recipes})
