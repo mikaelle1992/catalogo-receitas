df3758e47b5ebf6764cad245e692e1c8c6c7e8c9
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2025-03-26 16:49:06 -0300

    feature: classe RecipeViewsTest

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,12 +1,10 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve 
+from recipes import views 
 # Create your tests here.
 
 
 class RecipeUrlsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        ...
-    
     def test_recipe_home_url_is_correct(self):
         home_url = reverse('recipes:home')
         self.assertEquals(home_url, '/')
@@ -15,7 +13,20 @@ class RecipeUrlsTest(TestCase):
         category_url = reverse('recipes:category', kwargs={'category_id':1})
         self.assertEquals(category_url, '/recipes/category/1/')
 
-
     def test_recipe_detail_url_is_correct(self):
         recipes_url = reverse('recipes:recipe',kwargs={'id':1})
         self.assertEquals(recipes_url, '/recipes/1/')
+
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_view_function_is_correct(self):
+        view =  resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+    
+    def test_recipe_category_view_function_is_correct(self):
+        view =  resolve(reverse('recipes:category', kwargs={'category_id':1}))
+        self.assertIs(view.func, views.category)
+
+    def test_recipe_detail_view_function_is_correct(self):
+        view =  resolve(reverse('recipes:recipe', kwargs={'id':1}))
+        self.assertIs(view.func, views.recipe)
+
