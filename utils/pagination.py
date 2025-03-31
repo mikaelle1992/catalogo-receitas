56f826fef591a54ac9790af7c5f8dcebbfae085f
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2025-03-31 14:24:16 -0300

    feature: teste e função de paginação

---
+++ utils/pagination.py
@@ -0,0 +1,15 @@
+import math
+
+
+def make_pagination_range(page_range,qtd_paginas,current_page):
+    middle_range = math.ceil(qtd_paginas /2)
+    start_range = current_page - middle_range
+    stop_range = current_page + middle_range
+
+    start_range_offset = abs(start_range) if start_range < 0 else 0
+
+    if start_range < 0:
+        start_range = 0
+        stop_range += start_range_offset
+
+    return page_range[start_range:stop_range]
