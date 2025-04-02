3684d6913fa678540ddc0f08eb83f701c700836c
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2025-04-02 15:56:11 -0300

    feature: logout

--- authors/views.py
+++ authors/views.py
@@ -1,9 +1,11 @@
 from django.http import Http404
 from django.shortcuts import redirect, render
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 # Create your views here.
 from .forms import LoginForm, RegisterForm
+from django.contrib.auth.decorators import login_required
 
 def register_view(request):
    register_form_data = request.session.get('register_form_data', None)
@@ -49,17 +51,33 @@ def login_create(request):
    if not request.POST:
       raise Http404()
 
-   POST = request.POST
-   request.session['register_form_data'] = POST
-   form = RegisterForm(request.POST)
-
+   form = LoginForm(request.POST)
+   login_url = reverse('authors:login')
    if form.is_valid():
+      authenticated_user = authenticate(
+         username=form.cleaned_data.get('username', ''),
+         password=form.cleaned_data.get('password', ''),
+         )
 
-      user = form.save(commit=False)
-      user.set_password(user.password)
-      user.save()
-      
-      messages.success(request, 'Your user is created, please log in.')
-      del(request.session['register_form_data'])
+      if authenticated_user is not None:
+         messages.success(request, 'Your are logged in.')
+         login(request, authenticated_user)
+      else:
+         messages.error(request, 'Invalid credentials.')
+   else:
+      messages.error(request, 'Invalid username or password.')
 
-   return redirect('authors:register')
+   return redirect(login_url)
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def logout_view(request):
+   if not request.POST:
+      return redirect(reverse('authors:login'))
+   
+
+   if request.POST.get('username') != request.user.username:
+      return redirect(reverse('authors:login'))
+
+   logout(request)
+   return redirect(reverse('authors:login'))
