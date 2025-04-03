2c48bb3c889f7dff176894154f0c958c33fc3004
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2025-04-03 12:13:12 -0300

    feature: save forms

--- authors/views.py
+++ authors/views.py
@@ -113,9 +113,22 @@ def dashboard_edit_recipe(request, id):
       
       form = AuthorRecipeForm(
          data= request.POST or None,
+         files=request.FILES or None,
          instance=recipe
 
       )
+
+      if form.is_valid():
+         recipe = form.save(commit=False)
+
+         recipe.author = request.user
+         recipe.preparation_steps_is_html = False
+         recipe.is_published = False
+
+         recipe.save()
+
+         messages.success(request, 'Your recipes has been saved.')
+         return redirect(reverse('authors:dashboard_edit_recipe', args=(id,)))
 
       return render(request,'authors/pages/dashboard_recipe.html',
                   context={
