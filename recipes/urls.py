0b0558f8ec595d0f30f4a12f96ec59a0459499ef
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2025-04-04 11:33:03 -0300

    feature: JsonResponse

--- recipes/urls.py
+++ recipes/urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
     path('recipes/category/<int:category_id>/', views.RecipeListViewCatecory.as_view(), name="category"),
     path('recipes/<int:pk>/', views.RecipeDetail.as_view(), name="recipe"),
 
+
+    path('api/v1/', views.RecipeListViewHomeApi.as_view(), name="home_api"),
+    path('api/v1/recipes/<int:pk>/', views.RecipeDetailApi.as_view(), name="recipe_detail_api"),
+    # path('recipes/search/', views.RecipeListViewSearch.as_view(), name="search"),
+    # path('recipes/category/<int:category_id>/', views.RecipeListViewCatecory.as_view(), name="category"),
+
 ]
--- setup/settings.py
+++ setup/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'base_static',
 ]
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL =  '/media/'
-MEDIA_ROOT = BASE_DIR /'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
