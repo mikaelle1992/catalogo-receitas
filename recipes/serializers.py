02ac433abab2ac5b9e3fc3a426a01e7b2e147df6
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2025-04-05 14:12:31 -0300

    feature: category e author

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 class RecipeSerializer(serializers.Serializer):
@@ -7,12 +8,18 @@ class RecipeSerializer(serializers.Serializer):
     public = serializers.BooleanField(source='is_published') #renomeando o nome do campo
     preparation = serializers.SerializerMethodField()
     serving = serializers.SerializerMethodField()
+    # category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
+    category = serializers.StringRelatedField()
+    author = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all()
+    )
 
     def get_preparation(self, recipe):
         return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
     
     def get_serving(self, recipe):
         return f'{recipe.servings} {recipe.servings_unit}'
+    
     
     # preparation_time = serializers.IntegerField()
     # preparation_time_unit = serializers.CharField(max_length=65)
@@ -25,4 +32,4 @@ class RecipeSerializer(serializers.Serializer):
     # cover = serializers.
     # category = serializers.
     # on_dele = serializers.
-    # author = serializers.
+    # 
