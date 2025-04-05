3df165d6ad6a3f4d3fc7ce4afc9c14aace71beab
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2025-04-05 13:50:13 -0300

    feature: manupulação de campos no serializer

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -4,6 +4,16 @@ class RecipeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=165)
     description = serializers.CharField(max_length=165)
+    public = serializers.BooleanField(source='is_published') #renomeando o nome do campo
+    preparation = serializers.SerializerMethodField()
+    serving = serializers.SerializerMethodField()
+
+    def get_preparation(self, recipe):
+        return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
+    
+    def get_serving(self, recipe):
+        return f'{recipe.servings} {recipe.servings_unit}'
+    
     # preparation_time = serializers.IntegerField()
     # preparation_time_unit = serializers.CharField(max_length=65)
     # servings = serializers.IntegerField()
@@ -12,7 +22,6 @@ class RecipeSerializer(serializers.Serializer):
     # preparation_steps_is_html = serializers.BooleanField()
     # created_at = serializers.
     # updated_at = serializers.
-    # is_published = serializers.
     # cover = serializers.
     # category = serializers.
     # on_dele = serializers.
