b3526b3afdf7157e365dd47a1952199512f6a5d8
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2025-04-07 16:15:06 -0300

    feature: ModelViewSet

--- recipes/urls.py
+++ recipes/urls.py
@@ -19,8 +19,15 @@ urlpatterns = [
 
 
     # api rest
-    path('recipes/api/v2/',views.RecipeAPIv2List.as_view(), name='recipe_api_list'),
-    path('recipes/api/v2/<int:pk>/', views.RecipeAPIv2Detail.as_view(), name="recipe_detail_api_v2"),
+    path('recipes/api/v2/',views.RecipeAPIv2ModelViewSet.as_view({
+        'get':'list',
+        'post':'create'
+    }), name='recipe_api_list'),
+    path('recipes/api/v2/<int:pk>/', views.RecipeAPIv2ModelViewSet.as_view({
+        'get':'retrieve',
+        'patch':'partial_update',
+        'delete':'destroy',
+    }), name="recipe_detail_api_v2"),
     path('recipes/api/v2/tag/<int:pk>', views.recipe_api_tag, name="recipe_tag_api_v2"),
 
 ]
--- recipes/views/api.py
+++ recipes/views/api.py
@@ -3,23 +3,15 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from recipes.serializers import RecipeSerializer
 from recipes.models import Recipe
-from rest_framework import status
 from tag.models import Tag
 from tag.serializers import TagSerializer
-from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import PageNumberPagination
 
 class RecipeApiV2Pagination(PageNumberPagination):
     page_size = 4
 
-class RecipeAPIv2List(ListCreateAPIView):
-    queryset = Recipe.objects.get_published()
-    serializer_class = RecipeSerializer
-    pagination_class = RecipeApiV2Pagination
-
-
-class RecipeAPIv2Detail(RetrieveUpdateDestroyAPIView):
+class RecipeAPIv2ModelViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeApiV2Pagination
