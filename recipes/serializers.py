d7334b7995493413b0b48783e5516587f47df968
05792803579 <mrubia@apoioprodesp.sp.gov.br>
2025-04-07 10:25:49 -0300

    feature: ModelSerializer

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,32 +1,33 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from recipes.models import Recipe
 from tag.models import Tag
 from tag.serializers import TagSerializer
 
-class RecipeSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=165)
-    description = serializers.CharField(max_length=165)
-    public = serializers.BooleanField(source='is_published') #renomeando o nome do campo
-    preparation = serializers.SerializerMethodField()
-    serving = serializers.SerializerMethodField()
-    # category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
-    category = serializers.StringRelatedField()
-    author = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.all()
-    )
-    tags = serializers.PrimaryKeyRelatedField(
-        queryset=Tag.objects.all(),
-        many=True
-    )
+class RecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = [
+            'id', 'title', 'description', 'author',
+             'category', 'tags', 'public', 'preparation', 'serving',
+             'tag_objects', 'tag_links',
+             
+            ]
+
+    public = serializers.BooleanField(source='is_published', read_only= True) #renomeando o nome do campo
+    preparation = serializers.SerializerMethodField(read_only=True,)
+    serving = serializers.SerializerMethodField(read_only=True,)
+    category = serializers.StringRelatedField(read_only=True,)
+
     tag_objects = TagSerializer(
-        many = True, source = 'tags'
+        many = True, source = 'tags', read_only=True,
     )
     tag_links = serializers.HyperlinkedRelatedField(
         many = True, 
         source = 'tags', 
-        queryset=Tag.objects.all(),
+        # queryset=Tag.objects.all(),
+        read_only=True,
         view_name = 'recipes:recipe_tag_api_v2'
 
     )
@@ -38,11 +39,3 @@ class RecipeSerializer(serializers.Serializer):
         return f'{recipe.servings} {recipe.servings_unit}'
     
     
-    # preparation_steps = serializers.t
-    # preparation_steps_is_html = serializers.BooleanField()
-    # created_at = serializers.
-    # updated_at = serializers.
-    # cover = serializers.
-    # category = serializers.
-    # on_dele = serializers.
-    # 
--- tag/serializers.py
+++ tag/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 
-class TagSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField(max_length=165)
-    slug = serializers.SlugField()
+from tag.models import Tag
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag 
+        fields = ['id', 'name', 'slug']
+    # id = serializers.IntegerField()
+    # name = serializers.CharField(max_length=165)
+    # slug = serializers.SlugField()
